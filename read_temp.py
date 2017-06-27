20d2b2f6b46da01936a407b1d67bc8487a3ea81e
Olav Rossøy <olavvv@gmail.com>
2017-06-27 22:47:54 +0200

    test

--- read_temp.py
+++ read_temp.py
@@ -1,23 +1,25 @@
-from socketIO_client import SocketIO, LoggingNamespace
 from envirophat import weather
 import time
+from socketIO_client import SocketIO, LoggingNamespace
+with SocketIO('localhost', 8000, LoggingNamespace) as socketIO:
+    # socketIO.wait(seconds=1)
+    socketIO.on('connect', on_connect)
+    socketIO.on('disconnect', on_disconnect)
+    socketIO.on('reconnect', on_reconnect)
+
+    while True:
+        temp = weather.temperature()
+        socketIO.emit(temp)
+        time.sleep(1)
+
 
 def on_connect():
     print('connect')
 
+
 def on_disconnect():
     print('disconnect')
 
+
 def on_reconnect():
     print('reconnect')
-
-socketIO = SocketIO('10.0.0.114', 3000, LoggingNamespace)
-socketIO.on('connect', on_connect)
-socketIO.on('disconnect', on_disconnect)
-socketIO.on('reconnect', on_reconnect)
-
-# socketIO.wait(seconds=1)
-while True:
-    temp = weather.temperature()
-    socketIO.emit(temp)
-    time.sleep(1)
