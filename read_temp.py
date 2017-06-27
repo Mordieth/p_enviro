f22d4bd7074a1ce2cab43416b8299815e9dc0d45
Olav Rossøy <olavvv@gmail.com>
2017-06-27 23:11:56 +0200

    ok

--- read_temp.py
+++ read_temp.py
@@ -5,6 +5,6 @@ import time
 while True:
     myTemp = weather.temperature()
     print(myTemp)
-    payload = {temperature: myTemp}
+    payload = {'temperature': myTemp}
     requests.get('http://10.0.0.114:3000', params=payload)
     time.sleep(1)
