11acf3128b7c08cbb895a452159d536ee65a4fa2
Olav Rossøy <olavvv@gmail.com>
2017-06-27 22:18:10 +0200

    seperate server and python

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var io = require('socket.io')(app)
 app.listen(3000)
 
 function handler(req, res) {
+    console.log('request!')
     res.writeHead(200)
     res.end(data)
 }
 
 io.on('connection', function (socket) {
+    console.log('connection!')
     socket.on('*', function (data) {
         console.log(data)
     });
